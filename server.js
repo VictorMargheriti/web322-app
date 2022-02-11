68c67118a47b7ea64495eaee76a03ffe8388603f
VictorMargheriti <64934876+chikkinnuggaz@users.noreply.github.com>
2022-02-10 21:54:39 -0500

    P1+P2 (up to step 2)

--- server.js
+++ server.js
@@ -13,8 +13,19 @@
  ********************************************************************************/
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+const upload = multer(); // no { storage: storage } 
 const blogDataService = require("./blog-service.js");
 const app = express();
+
+cloudinary.config({
+  cloud_name: 'dfknzgo2m',
+  api_key: '421913683339951',
+  api_secret: '3hhtHIxES2TMuu3iIRIFUzBM3Sg',
+  secure: true
+ });
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -62,6 +73,52 @@ app.get("/categories", (req, res) => {
     });
 });
 
+app.get("/posts/add", (req, res)=>{
+  res.sendFile(path.join(__dirname, "views/addPost.html"))
+});
+
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+  if(req.file){
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+
+    upload(req).then((uploaded)=>{
+        processPost(uploaded.url);
+    });
+}else{
+    processPost("");
+}
+
+function processPost(imageUrl){
+    req.body.featureImage = imageUrl;
+    res.send(`
+        <h1>${req.body.title}</h1><br />
+        <img src="${imageUrl}" /><br />
+        <p>${req.body.body}</p>
+    `);
+    res.redirect("views/posts");
+} 
+});
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
 });
@@ -76,3 +133,7 @@ blogDataService
   .catch((err) => {
     console.log(err);
   });
+
+
+
+  
